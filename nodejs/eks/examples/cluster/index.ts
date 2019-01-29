9acccb3269c261c74fae5b4455ced80460a4105a
Luke Hoban <luke@pulumi.com>
2019-01-29 12:17:33 -0800

    Add test coverage for non-default Cluster configuration
    Covers:
    1. Custom VPC with private subnets
    2. Custom ASG sizing
    3. Not deploying dashboard

--- nodejs/eks/examples/cluster/index.ts
+++ nodejs/eks/examples/cluster/index.ts
@@ -1,7 +1,20 @@
+import * as awsinfra from "@pulumi/aws-infra";
 import * as eks from "@pulumi/eks";
 
 // Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("cluster");
+const cluster1 = new eks.Cluster("cluster1");
 
-// Export the cluster's kubeconfig.
-export const kubeconfig = cluster.kubeconfig;
+// Create an EKS cluster with non-default configuration
+const vpc = new awsinfra.Network("vpc", { usePrivateSubnets: true });
+const cluster2 = new eks.Cluster("cluster2", {
+    vpcId: vpc.vpcId,
+    subnetIds: vpc.subnetIds,
+    desiredCapacity: 5,
+    minSize: 5,
+    maxSize: 10,
+    deployDashboard: false,
+});
+
+// Export the clusters' kubeconfig.
+export const kubeconfig1 = cluster1.kubeconfig;
+export const kubeconfig2 = cluster2.kubeconfig;
