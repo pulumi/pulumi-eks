91a142d598cc8c8e6c896785391f8496ea7658c7
Mike Metral <1112768+metral@users.noreply.github.com>
2019-03-28 21:19:24 -0700

    example(priv-cluster): reduce num of AZ's from 3 -> 2

--- nodejs/eks/examples/private-cluster/index.ts
+++ nodejs/eks/examples/private-cluster/index.ts
@@ -5,7 +5,7 @@ const name = "private-cluster";
 
 // Create a VPC that uses private subnets.
 const network = new awsx.Network(name, {
-    numberOfAvailabilityZones: 3,
+    numberOfAvailabilityZones: 2,
     usePrivateSubnets: true,
 });
 
