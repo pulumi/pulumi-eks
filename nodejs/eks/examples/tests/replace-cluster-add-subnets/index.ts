41a1bbce8865341d833de64586d6db6db0ba0190
Mike Metral <1112768+metral@users.noreply.github.com>
2019-05-10 14:54:04 -0700

    test(add-subnets): replace cluster after adding more subnets to use
    Note: this test works, but it is currently disabled until issue 107 is resolved.

---
+++ nodejs/eks/examples/tests/replace-cluster-add-subnets/index.ts
@@ -0,0 +1,24 @@
+import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks";
+import * as pulumi from "@pulumi/pulumi";
+
+const projectName = pulumi.getProject();
+
+const vpc = new awsx.Network(`${projectName}-net`, {
+    numberOfAvailabilityZones: 3,
+});
+
+const publicSubnetIds = vpc.publicSubnetIds;
+
+// Comment out / remove after init update to repro: https://git.io/fj8cn
+publicSubnetIds.pop();
+
+const cluster = new eks.Cluster(projectName, {
+    vpcId: vpc.vpcId,
+    subnetIds: publicSubnetIds,
+    deployDashboard: false,
+});
+
+// Export the cluster name and kubeconfig
+export const clusterName = cluster.core.cluster.name;
+export const kubeconfig = cluster.kubeconfig;
