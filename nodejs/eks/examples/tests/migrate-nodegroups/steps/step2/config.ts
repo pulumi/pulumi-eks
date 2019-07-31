3b9d6a81109b600ceff976049e3aa25990df4fd8
Mike Metral <1112768+metral@users.noreply.github.com>
2019-07-31 15:54:45 -0700

    fix(test): sleep after migration, and remove the workload & CNI

--- nodejs/eks/examples/tests/migrate-nodegroups/steps/step2/config.ts
+++ nodejs/eks/examples/tests/migrate-nodegroups/steps/step2/config.ts
@@ -1,8 +1,9 @@
-// Enable the migration of NGINX from the 2xlarge -> 4xlarge node group.
+// Migrate NGINX from the 2xlarge -> 4xlarge node group.
 export const config = {
     createNodeGroup2xlarge: true,
     desiredCapacity2xlarge: 3,
     createNodeGroup4xlarge: true,
     desiredCapacity4xlarge: 5,
     nginxNodeSelectorTermValues: ["c5.4xlarge"],
+    deployWorkload: true,
 };
