e732065719b1f4e08e656a0714ceab82c97ef415
Mike Metral <1112768+metral@users.noreply.github.com>
2020-01-27 12:15:58 -0800

    test(migrate-ng): fix compile err from not using defined object in config

--- nodejs/eks/examples/tests/migrate-nodegroups/steps/step5/config.ts
+++ nodejs/eks/examples/tests/migrate-nodegroups/steps/step5/config.ts
@@ -1,8 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+
+interface Config {
+    createNodeGroup2xlarge: pulumi.Input<boolean>;
+    desiredCapacity2xlarge: pulumi.Input<number>;
+
+    createNodeGroup4xlarge: pulumi.Input<boolean>;
+    desiredCapacity4xlarge: pulumi.Input<number>;
+
+    deployWorkload: pulumi.Input<boolean>;
+    nginxNodeSelectorTermValues: pulumi.Input<string>[];
+}
+
 // Remove the 2xlarge node group.
-export const config = {
+export const config: Config = {
     createNodeGroup2xlarge: false,
     desiredCapacity2xlarge: 0,
     createNodeGroup4xlarge: true,
     desiredCapacity4xlarge: 5,
     deployWorkload: false,
+    nginxNodeSelectorTermValues: [],
 };
