742fa46bf4ce94df7ffffc7cdb21a271b11fca3f
Mike Metral <1112768+metral@users.noreply.github.com>
2020-01-27 12:15:58 -0800

    test(migrate-ng): fix compile err from not using defined object in config

--- nodejs/eks/examples/tests/migrate-nodegroups/steps/step3/config.ts
+++ nodejs/eks/examples/tests/migrate-nodegroups/steps/step3/config.ts
@@ -1,8 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+
+interface Config {
+    createNodeGroup2xlarge: pulumi.Input<boolean>;
+    desiredCapacity2xlarge: pulumi.Input<number>;
+
+    createNodeGroup4xlarge: pulumi.Input<boolean>;
+    desiredCapacity4xlarge: pulumi.Input<number>;
+
+    deployWorkload: pulumi.Input<boolean>;
+    nginxNodeSelectorTermValues: pulumi.Input<string>[];
+}
+
 // Remove the NGINX & echoserver workload.
-export const config = {
+export const config: Config = {
     createNodeGroup2xlarge: true,
     desiredCapacity2xlarge: 3,
     createNodeGroup4xlarge: true,
     desiredCapacity4xlarge: 5,
     deployWorkload: false,
+    nginxNodeSelectorTermValues: [],
 };
--- nodejs/eks/examples/tests/migrate-nodegroups/steps/step4/config.ts
+++ nodejs/eks/examples/tests/migrate-nodegroups/steps/step4/config.ts
@@ -1,8 +1,22 @@
+import * as pulumi from "@pulumi/pulumi";
+
+interface Config {
+    createNodeGroup2xlarge: pulumi.Input<boolean>;
+    desiredCapacity2xlarge: pulumi.Input<number>;
+
+    createNodeGroup4xlarge: pulumi.Input<boolean>;
+    desiredCapacity4xlarge: pulumi.Input<number>;
+
+    deployWorkload: pulumi.Input<boolean>;
+    nginxNodeSelectorTermValues: pulumi.Input<string>[];
+}
+
 // Scale down the 2xlarge node group to a desired capacity of 0 workers.
-export const config = {
+export const config: Config = {
     createNodeGroup2xlarge: true,
     desiredCapacity2xlarge: 0,
     createNodeGroup4xlarge: true,
     desiredCapacity4xlarge: 5,
     deployWorkload: false,
+    nginxNodeSelectorTermValues: [],
 };
