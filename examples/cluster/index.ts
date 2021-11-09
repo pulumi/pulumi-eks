64b280d4c679c7b7d70ece542127ab5405f04851
Vivek Lakshmanan <vivek@lakshmanan.ca>
2021-11-08 22:14:41 -0800

    Add test for init container and disable TCP early demux

--- examples/cluster/index.ts
+++ examples/cluster/index.ts
@@ -9,7 +9,7 @@ const cluster1 = new eks.Cluster(`${projectName}-1`);
 
 // Create an EKS cluster with a non-default configuration.
 const vpc = new awsx.ec2.Vpc(`${projectName}-2`, {
-    tags: { "Name": `${projectName}-2` },
+    tags: {"Name": `${projectName}-2`},
 });
 
 const cluster2 = new eks.Cluster(`${projectName}-2`, {
@@ -27,6 +27,7 @@ const cluster2 = new eks.Cluster(`${projectName}-2`, {
     vpcCniOptions: {
         image: "602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon-k8s-cni:v1.9.0",
         initImage: "602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon-k8s-cni-init:v1.9.0",
+        disableTcpEarlyDemux: true,
     },
 });
 
