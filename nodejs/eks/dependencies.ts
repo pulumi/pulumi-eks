c8fc6feea69bde30a2ca19a5afe7b6b63aa5245d
Vivek Lakshmanan <vivek@lakshmanan.ca>
2022-06-16 17:55:50 -0700

    Add checks to validate versions of kubectl and aws-cli installed

---
+++ nodejs/eks/dependencies.ts
@@ -0,0 +1,76 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import which = require("which");
+import * as semver from "semver";
+import * as childProcess from "child_process";
+
+const minKubectlVersion = '1.24.0';
+const minAWSCLIV1Version = '1.24.0';
+const minAWSCLIV2Version = '2.7.0';
+
+interface kubectlVersion {
+    clientVersion: {
+        major: string;
+        minor: string;
+        gitVersion: string;
+    }
+}
+
+export function assertCompatibleKubectlVersionExists() {
+    try {
+        which.sync("kubectl");
+    } catch (err) {
+        throw new Error("kubectl is missing. See https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl for installation instructions.");
+    }
+
+    let kubectlVersionJson: string;
+    try {
+        kubectlVersionJson = childProcess.execSync(`kubectl version --output=json`, { stdio: ['pipe', 'pipe', 'ignore'], encoding: "utf8" });
+    } catch (err) {
+        kubectlVersionJson = err.stdout // Might see partial failure if the the remote cluster is inaccessible. Ignore. 
+    }
+
+    const kubectlVersion: kubectlVersion = JSON.parse(kubectlVersionJson);
+    const kcVersion = semver.clean(kubectlVersion.clientVersion.gitVersion, {loose: true, includePrerelease: true});
+    if (semver.lt(kcVersion!, minKubectlVersion)) {
+        throw new Error(`At least v${minKubectlVersion} of kubectl is required. Current version is: ${kcVersion}. See https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl for instructions on installing the latest.`)
+    }
+}
+
+export function assertCompatibleAWSCLIExists() {
+    try {
+        which.sync("aws");
+    } catch (err) {
+        throw new Error("Could not find aws CLI for EKS. See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html for installation instructions.");
+    }
+
+    const awscli = childProcess.execSync(`aws --version`, { stdio: ['pipe', 'pipe', 'ignore'], encoding: "utf8" });
+    const version = awscli.split(' ')[0].replace("aws-cli/", "");
+    const semverCleaned = semver.clean(version, {loose: true, includePrerelease: true});
+    switch (semver.major(semverCleaned!)) {
+        case 1:
+            if (semver.lt(semverCleaned!, minAWSCLIV1Version)) {
+                throw new Error(`At least v${minAWSCLIV1Version} of aws-cli is required.` + 
+                ` Current version is: ${semverCleaned}. See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html for installation instructions.`)
+            }
+            break;
+        default:
+            if (semver.lt(semverCleaned!, minAWSCLIV2Version)) {
+                throw new Error(`At least v${minAWSCLIV2Version} of aws-cli is required.` + 
+                ` Current version is: ${semverCleaned}. See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html for installation instructions.`)
+            }
+            break;
+    }
+}
