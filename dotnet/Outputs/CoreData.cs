0d772747116a8154118e341f8a58a0149f38a6c0
stack72 <public@paulstack.co.uk>
2022-07-06 22:45:32 +0100

    Export the ARN for the IAM Role attached to the EKS Cluster
    Fixes: #727
    
    In #670, we removed the deprecated IAM Role Policy `AmazonEKSServicePolicy`.
    Unfortunately, this has broken clusters older than 16th April 2020
    
    Rather than adding this policy back by default, we now expose the
    ARN of the IAM Role for the cluster so that a user can do as follows:
    
    ```
    const cluster1 = new eks.Cluster(`${projectName}-1`);
    
    const iamRole = cluster1.clusterIamRole;
    
    const rpa = new aws.iam.RolePolicyAttachment("rpa", {
      role: iamRole.name,
      policy: "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
    })
    ```

--- dotnet/Outputs/CoreData.cs
+++ dotnet/Outputs/CoreData.cs
@@ -18,6 +18,10 @@ namespace Pulumi.Eks.Outputs
     {
         public readonly Pulumi.Aws.Provider? AwsProvider;
         public readonly Pulumi.Aws.Eks.Cluster Cluster;
+        /// <summary>
+        /// The IAM Role attached to the EKS Cluster
+        /// </summary>
+        public readonly Pulumi.Aws.Iam.Role ClusterIamRole;
         public readonly Pulumi.Aws.Ec2.SecurityGroup ClusterSecurityGroup;
         public readonly Pulumi.Kubernetes.Core.V1.ConfigMap? EksNodeAccess;
         public readonly Pulumi.Aws.Eks.Outputs.ClusterEncryptionConfig? EncryptionConfig;
@@ -42,6 +46,8 @@ namespace Pulumi.Eks.Outputs
             Pulumi.Aws.Provider? awsProvider,
 
             Pulumi.Aws.Eks.Cluster cluster,
+
+            Pulumi.Aws.Iam.Role clusterIamRole,
 
             Pulumi.Aws.Ec2.SecurityGroup clusterSecurityGroup,
 
@@ -81,6 +87,7 @@ namespace Pulumi.Eks.Outputs
         {
             AwsProvider = awsProvider;
             Cluster = cluster;
+            ClusterIamRole = clusterIamRole;
             ClusterSecurityGroup = clusterSecurityGroup;
             EksNodeAccess = eksNodeAccess;
             EncryptionConfig = encryptionConfig;
