f5f4ea8b9ed06ad89c0e240e2f1010c61c95b1ee
Daniel Bradley <daniel@pulumi.com>
2022-10-17 16:13:34 +0100

    Add test of NodeGroupV2 to python test
    - Add non-typescript test coverage.

--- examples/cluster-py/__main__.py
+++ examples/cluster-py/__main__.py
@@ -14,22 +14,30 @@ cluster1 = eks.Cluster(f"{project_name}-1")
 vpc = Vpc(f"{project_name}-2")
 
 cluster2 = eks.Cluster('eks-cluster',
-                          vpc_id=vpc.vpc_id,
-                          public_subnet_ids=vpc.public_subnet_ids,
-                          public_access_cidrs=['0.0.0.0/0'],
-                          desired_capacity=2,
-                          min_size=2,
-                          max_size=2,
-                          instance_type='t3.micro',
-                          # set storage class.
-                          storage_classes={"gp2": eks.StorageClassArgs(
-                              type='gp2', allow_volume_expansion=True, default=True, encrypted=True,)},
-                          enabled_cluster_log_types=[
-                              "api",
-                              "audit",
-                              "authenticator",
-                          ],)
+                       vpc_id=vpc.vpc_id,
+                       public_subnet_ids=vpc.public_subnet_ids,
+                       public_access_cidrs=['0.0.0.0/0'],
+                       desired_capacity=2,
+                       min_size=2,
+                       max_size=2,
+                       instance_type='t3.micro',
+                       # set storage class.
+                       storage_classes={"gp2": eks.StorageClassArgs(
+                           type='gp2', allow_volume_expansion=True, default=True, encrypted=True,)},
+                       enabled_cluster_log_types=[
+                           "api",
+                           "audit",
+                           "authenticator",
+                       ],)
 
+
+node_group_2 = eks.NodeGroupV2("node-group-v2",
+                               cluster=cluster2,
+                               instance_type="t2.medium",
+                               desired_capacity=1,
+                               min_size=1,
+                               max_size=1,
+                               labels={"ondemand": "true"})
 
 # Export the clusters' kubeconfig.
 pulumi.export("kubeconfig1", cluster1.kubeconfig)
