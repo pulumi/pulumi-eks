57b526fdac0d8438c2c7c6acc821f4303958d4a7
Mikhail Shilkov <github@mikhail.io>
2023-06-20 12:58:42 +0200

    Revert "expose ipFamily" (#887)

--- sdk/dotnet/Cluster.cs
+++ sdk/dotnet/Cluster.cs
@@ -279,17 +279,10 @@ namespace Pulumi.Eks
         public Input<string>? InstanceType { get; set; }
 
         /// <summary>
-        /// The IP family used to assign Kubernetes pod and service addresses. Valid values are `ipv4` (default) and `ipv6`.
-        /// You can only specify an IP family when you create a cluster, changing this value will force a new cluster to be created.
-        /// </summary>
-        [Input("ipFamily")]
-        public Input<string>? IpFamily { get; set; }
-
-        /// <summary>
         /// The CIDR block to assign Kubernetes service IP addresses from. If you don't
         /// specify a block, Kubernetes assigns addresses from either the 10.100.0.0/16 or
-        /// 172.20.0.0/16 CIDR blocks. This setting only applies to IPv4 clusters. We recommend that you specify a block
-        /// that does not overlap with resources in other networks that are peered or connected to your VPC. You can only specify
+        /// 172.20.0.0/16 CIDR blocks. We recommend that you specify a block that does not overlap
+        /// with resources in other networks that are peered or connected to your VPC. You can only specify
         /// a custom CIDR block when you create a cluster, changing this value will force a new cluster to be created.
         /// 
         /// The block must meet the following requirements:
