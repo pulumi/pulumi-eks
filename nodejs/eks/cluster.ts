c66cbd8b3226b459e3cd268444c5ccbf148baf8e
Thomas Kappler <tkappler@pulumi.com>
2023-09-05 06:32:15 +0200

    Upgrade to aws v6.0.4 for the fix for aws#2774

--- nodejs/eks/cluster.ts
+++ nodejs/eks/cluster.ts
@@ -313,8 +313,8 @@ export function getRoleProvider(
     parent?: pulumi.ComponentResource,
     provider?: pulumi.ProviderResource,
 ): CreationRoleProvider {
-    const partition = pulumi.output(aws.getPartition({ })).partition;
-    const accountId = pulumi.output(aws.getCallerIdentity({ })).accountId;
+    const partition = pulumi.output(aws.getPartition({})).partition;
+    const accountId = pulumi.output(aws.getCallerIdentity({})).accountId;
     const iamRole = new aws.iam.Role(
         `${name}-eksClusterCreatorRole`,
         {
@@ -449,7 +449,7 @@ export function createCore(
         version: args.version,
     };
 
-    const { partition, dnsSuffix } = pulumi.output(aws.getPartition({ }));
+    const { partition, dnsSuffix } = pulumi.output(aws.getPartition({}));
 
     // Configure default networking architecture.
     let vpcId: pulumi.Input<string> = args.vpcId!;
