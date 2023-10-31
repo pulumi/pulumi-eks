d337f7ae63c492183e61442e608ffc9e30edf04c
Thomas Kappler <tkappler@pulumi.com>
2023-10-31 15:03:01 +0100

    Update dependencies

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -20,27 +20,11 @@ func test(t *testing.T, dir string, opts ...providertest.Option) *providertest.P
 			providertest.UpgradeTestMode_Quick,
 			"Quick mode is only supported for providers written in Go at the moment"),
 
-		providertest.WithBaselineAmbientPlugins(
-			providertest.AmbientPlugin{
-				Provider: "eks",
-				Version:  "1.0.4",
-			},
-			providertest.AmbientPlugin{
-				Provider: "aws",
-				Version:  "5.42.0",
-			},
-		),
-
-		providertest.WithAmbientPlugins(
-			providertest.AmbientPlugin{
-				Provider:  "eks",
-				LocalPath: "../bin/pulumi-resource-eks",
-			},
-			providertest.AmbientPlugin{
-				Provider: "aws",
-				Version:  "6.0.2",
-			},
-		),
+		providertest.WithBaselineVersion("1.0.4"),
+		providertest.WithExtraBaselineDependencies(map[string]string{
+			"aws":        "5.42.0",
+			"kubernetes": "3.30.2",
+		}),
 	)
 	return providertest.NewProviderTest(dir, opts...)
 }
