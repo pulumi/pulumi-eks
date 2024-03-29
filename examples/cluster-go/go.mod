e55b0c2d1e11fcdb89a974c13a9bb7fe9f12a9c0
Ramon Quitales <ramon@pulumi.com>
2024-03-29 10:42:36 -0700

    Expose Ip-Family for IPV6 (#1089) (#1098)
    ### Proposed changes
    This PR exposes the `ipFamily` argument to enable creating EKS clusters
    that support ipv6 networking.
    Modified from https://github.com/pulumi/pulumi-eks/pull/888 and rebased
    on top of latest changes in master.
    
    An additional cluster with ipv6 is also created to test that cluster
    creation works as expected.
    
    ### Related issues (optional)
    Closes: #698
    Closes: #684
    
    ---------
    
    Co-authored-by: David Roth <david.roth@fusonic.net>
    Co-authored-by: Mikhail Shilkov <github@mikhail.io>
    Co-authored-by: Kyle Pitzen <kpitzen@pulumi.com>

--- examples/cluster-go/go.mod
+++ examples/cluster-go/go.mod
@@ -3,8 +3,9 @@ module github.com/pulumi/pulumi-eks/examples/cluster-go
 go 1.21
 
 require (
+	github.com/pulumi/pulumi-awsx/sdk/v2 v2.6.0
 	github.com/pulumi/pulumi-eks/sdk/v2 v2.0.0
-	github.com/pulumi/pulumi/sdk/v3 v3.91.1
+	github.com/pulumi/pulumi/sdk/v3 v3.93.0
 )
 
 require (
@@ -56,7 +57,8 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/term v1.1.0 // indirect
 	github.com/pulumi/esc v0.5.6 // indirect
-	github.com/pulumi/pulumi-aws/sdk/v6 v6.5.0 // indirect
+	github.com/pulumi/pulumi-aws/sdk/v6 v6.9.0 // indirect
+	github.com/pulumi/pulumi-docker/sdk/v4 v4.4.3 // indirect
 	github.com/pulumi/pulumi-kubernetes/sdk/v4 v4.4.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
@@ -66,7 +68,6 @@ require (
 	github.com/skeema/knownhosts v1.2.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/texttheater/golang-levenshtein v1.0.1 // indirect
 	github.com/tweekmonster/luser v0.0.0-20161003172636-3fa38070dbd7 // indirect
 	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
@@ -83,7 +84,7 @@ require (
 	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230731190214-cbb8c96f2d6d // indirect
 	google.golang.org/grpc v1.57.1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- examples/cluster-go/main.go
+++ examples/cluster-go/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
 	"github.com/pulumi/pulumi-eks/sdk/v2/go/eks"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -28,6 +32,8 @@ func main() {
 			return err
 		}
 
+		// Create a cluster that avoids conflicts between default settings and mutually exclusive arguments
+		// https://github.com/pulumi/pulumi-eks/pull/813
 		cluster3, err := eks.NewCluster(ctx, "example-cluster-3", &eks.ClusterArgs{
 			NodeGroupOptions: &eks.ClusterNodeGroupOptionsArgs{
 				DesiredCapacity: pulumi.IntPtr(2),
@@ -39,10 +45,76 @@ func main() {
 			return err
 		}
 
-		// Export the kubeconfig for clusters
+		//////////////////////////////////////////////////
+		///     Create an ipv6 enabled EKS cluster     ///
+		//////////////////////////////////////////////////
+
+		// 1. Create a VPC with IPv6 CIDR block.
+		vpc, err := ec2.NewVpc(ctx, "ipv6-vpc", &ec2.VpcArgs{
+			AssignGeneratedIpv6CidrBlock: pulumi.Bool(true),
+			EnableDnsSupport:             pulumi.Bool(true),
+			EnableDnsHostnames:           pulumi.Bool(true),
+			CidrBlock:                    pulumi.String("10.100.0.0/16"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// 2. Create two subnets with IPv6 CIDR block in two different availability zones.
+		//    EKS requires at least two subnets in different availability zones.
+		var subnetIDs []pulumi.StringInput
+		for idx, az := range []string{"us-west-2a", "us-west-2b"} {
+			subnet, err := ec2.NewSubnet(ctx, fmt.Sprintf("ipv6-subnet-%d", idx), &ec2.SubnetArgs{
+				VpcId:                       vpc.ID().ToStringOutput(),
+				AssignIpv6AddressOnCreation: pulumi.Bool(true),
+				AvailabilityZone:            pulumi.String(az),
+				CidrBlock:                   pulumi.String(fmt.Sprintf("10.100.%d.0/24", len(subnetIDs))),
+				Ipv6CidrBlock:               calculateIPV6CidrBlock(vpc.Ipv6CidrBlock, idx),
+			})
+			if err != nil {
+				return err
+			}
+
+			subnetIDs = append(subnetIDs, subnet.ID().ToStringOutput())
+		}
+
+		// 3. Create an EKS cluster with IPv6 networking enabled.
+		cluster4, err := eks.NewCluster(ctx, "example-cluster-4", &eks.ClusterArgs{
+			IpFamily:         pulumi.StringPtr("ipv6"),
+			VpcId:            vpc.ID().ToStringOutput(),
+			SubnetIds:        pulumi.StringArray(subnetIDs),
+			UseDefaultVpcCni: func() *bool { t := true; return &t }(),
+			NodeGroupOptions: &eks.ClusterNodeGroupOptionsArgs{
+				DesiredCapacity: pulumi.IntPtr(2),
+				MinSize:         pulumi.IntPtr(2),
+				MaxSize:         pulumi.IntPtr(2),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		//////////////////////////////////////////
+		///     Export cluster kubeconfigs     ///
+		//////////////////////////////////////////
+
 		ctx.Export("kubeconfig1", cluster1.Kubeconfig)
 		ctx.Export("kubeconfig2", cluster2.Kubeconfig)
 		ctx.Export("kubeconfig3", cluster3.Kubeconfig)
+		ctx.Export("kubeconfig4", cluster4.Kubeconfig)
 		return nil
 	})
+}
+
+// calculateIPV6CidrBlock is a very simple function to calculate the ipv6 subnet cidr block for testing purpose.
+// Usage for real workloads should implement a more robust function.
+// Example: If the VPC ipv6 cidr block is 2600:1f13:e6:c000::/56, then the subnet ipv6 cidr block will be:
+// 2600:1f13:e6:c005::/64, 2600:1f13:e6:c006::/64, 2600:1f13:e6:c007::/64, ...
+func calculateIPV6CidrBlock(ipv6CidrBlock pulumi.StringOutput, subnetID int) pulumi.StringInput {
+	return ipv6CidrBlock.ApplyT(func(cidr string) (string, error) {
+		cidrStripped := strings.TrimSuffix(cidr, "::/56")
+		cidrStripped = cidrStripped[:len(cidrStripped)-1]
+		return fmt.Sprintf("%s%d::/64", cidrStripped, subnetID+5), nil
+
+	}).(pulumi.StringOutput)
 }
