98c18727dac67e3dcf7bcb1bbc8d288956167dd3
Ramon Quitales <ramon@pulumi.com>
2024-05-16 13:59:22 -0700

    chore: bump baseline provider version for snapshot recording

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -14,7 +14,7 @@ import (
 
 const (
 	// The baseline version is the version of the provider that the upgrade tests will be run against.
-	baselineVersion = "2.3.0"
+	baselineVersion = "2.4.0"
 
 	// The path to the yarn.lock file that the EKS provider was built with.
 	yarnLockPath = "../nodejs/eks/yarn.lock"
