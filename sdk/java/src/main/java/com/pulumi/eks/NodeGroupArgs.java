e8843829db259621822a3034ac2a9b37f3043901
Florian Stadler <florian@pulumi.com>
2024-08-30 20:25:41 +0200

    Remove AL2 hardcoding to start supporting AL2023 and Bottlerocket (#1328)

--- sdk/java/src/main/java/com/pulumi/eks/NodeGroupArgs.java
+++ sdk/java/src/main/java/com/pulumi/eks/NodeGroupArgs.java
@@ -10,6 +10,7 @@ import com.pulumi.core.Either;
 import com.pulumi.core.Output;
 import com.pulumi.core.annotations.Import;
 import com.pulumi.eks.Cluster;
+import com.pulumi.eks.enums.OperatingSystem;
 import com.pulumi.eks.inputs.CoreDataArgs;
 import com.pulumi.eks.inputs.TaintArgs;
 import java.lang.Boolean;
@@ -584,6 +585,27 @@ public final class NodeGroupArgs extends com.pulumi.resources.ResourceArgs {
     }
 
     /**
+     * The type of OS to use for the node group. Will be used to determine the right EKS optimized AMI to use based on the instance types and gpu configuration.
+     * Valid values are `AL2`, `AL2023` and `Bottlerocket`.
+     * 
+     * Defaults to `AL2`.
+     * 
+     */
+    @Import(name="operatingSystem")
+    private @Nullable Output<OperatingSystem> operatingSystem;
+
+    /**
+     * @return The type of OS to use for the node group. Will be used to determine the right EKS optimized AMI to use based on the instance types and gpu configuration.
+     * Valid values are `AL2`, `AL2023` and `Bottlerocket`.
+     * 
+     * Defaults to `AL2`.
+     * 
+     */
+    public Optional<Output<OperatingSystem>> operatingSystem() {
+        return Optional.ofNullable(this.operatingSystem);
+    }
+
+    /**
      * Bidding price for spot instance. If set, only spot instances will be added as worker node.
      * 
      */
@@ -662,6 +684,7 @@ public final class NodeGroupArgs extends com.pulumi.resources.ResourceArgs {
         this.nodeSubnetIds = $.nodeSubnetIds;
         this.nodeUserData = $.nodeUserData;
         this.nodeUserDataOverride = $.nodeUserDataOverride;
+        this.operatingSystem = $.operatingSystem;
         this.spotPrice = $.spotPrice;
         this.taints = $.taints;
         this.version = $.version;
@@ -1420,6 +1443,33 @@ public final class NodeGroupArgs extends com.pulumi.resources.ResourceArgs {
          */
         public Builder nodeUserDataOverride(String nodeUserDataOverride) {
             return nodeUserDataOverride(Output.of(nodeUserDataOverride));
+        }
+
+        /**
+         * @param operatingSystem The type of OS to use for the node group. Will be used to determine the right EKS optimized AMI to use based on the instance types and gpu configuration.
+         * Valid values are `AL2`, `AL2023` and `Bottlerocket`.
+         * 
+         * Defaults to `AL2`.
+         * 
+         * @return builder
+         * 
+         */
+        public Builder operatingSystem(@Nullable Output<OperatingSystem> operatingSystem) {
+            $.operatingSystem = operatingSystem;
+            return this;
+        }
+
+        /**
+         * @param operatingSystem The type of OS to use for the node group. Will be used to determine the right EKS optimized AMI to use based on the instance types and gpu configuration.
+         * Valid values are `AL2`, `AL2023` and `Bottlerocket`.
+         * 
+         * Defaults to `AL2`.
+         * 
+         * @return builder
+         * 
+         */
+        public Builder operatingSystem(OperatingSystem operatingSystem) {
+            return operatingSystem(Output.of(operatingSystem));
         }
 
         /**
