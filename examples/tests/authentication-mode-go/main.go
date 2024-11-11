eb0b644aafe7d10668bc5f0e4953a88f02e9fcce
Florian Stadler <florian@pulumi.com>
2024-11-11 20:02:37 +0100

    Add regression test for access entry output

---
+++ examples/tests/authentication-mode-go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-eks/sdk/v3/go/eks"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		authMode := eks.AuthenticationModeApi
+		cluster, err := eks.NewCluster(ctx, "example-managed-nodegroups-go", &eks.ClusterArgs{
+			AuthenticationMode: &authMode,
+		})
+		if err != nil {
+			return err
+		}
+
+		accessEntries := cluster.Core.AccessEntries()
+		ctx.Export("accessEntries", accessEntries)
+
+		// Export the kubeconfig for the cluster
+		ctx.Export("kubeconfig", cluster.Kubeconfig)
+
+		return nil
+	})
+}
