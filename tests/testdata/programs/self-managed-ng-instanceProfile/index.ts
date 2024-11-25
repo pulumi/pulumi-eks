4139df2d8a86270a936dccbe3948cd99dff84c7c
Jonathan Davenport <jdavenport@pulumi.com>
2024-11-25 09:31:24 -0500

    removes extraneous nodegroup/v2 test cases

--- tests/testdata/programs/self-managed-ng-instanceProfile/index.ts
+++ tests/testdata/programs/self-managed-ng-instanceProfile/index.ts
@@ -32,76 +32,13 @@ const cluster_NoIp_NoIpn = new eks.Cluster("cluster-noIp-noIpn", {
       type: eks.AccessEntryType.EC2Linux,
     }
   },
-  // Needed for AL2023 until the CNI is managed with an addon because our custom CNI is too old
-  useDefaultVpcCni: true,
 })
-
-// Cluster that is passed an instanceProfile via nodeGroupOptions
-// and that does not create a default NodeGroupV2
-const cluster_instanceProfileFromNgOpts = new eks.Cluster("cluster-instanceProfileFromNgOpts", {
-  nodeGroupOptions: {
-    instanceProfile: instanceProfile
-  },
-  skipDefaultNodeGroup: true,
-  vpcId: eksVpc.vpcId,
-  authenticationMode: eks.AuthenticationMode.Api,
-  // Public subnets will be used for load balancers
-  publicSubnetIds: eksVpc.publicSubnetIds,
-  // Private subnets will be used for cluster nodes
-  privateSubnetIds: eksVpc.privateSubnetIds,
-  accessEntries: {
-    instances: {
-      principalArn: role.arn,
-      type: eks.AccessEntryType.EC2Linux,
-    }
-  },
-  // Needed for AL2023 until the CNI is managed with an addon because our custom CNI is too old
-  useDefaultVpcCni: true,
-})
-
-// Cluster that is passed an instanceProfileName via nodeGroupOptions
-// and that does not create a default NodeGroupV2
-const cluster_instanceProfileNameFromNgOpts = new eks.Cluster("cluster-instanceProfileNameFromNgOpts", {
-  nodeGroupOptions: {
-    instanceProfileName: instanceProfile.name
-  },
-  skipDefaultNodeGroup: true,
-  vpcId: eksVpc.vpcId,
-  authenticationMode: eks.AuthenticationMode.Api,
-  // Public subnets will be used for load balancers
-  publicSubnetIds: eksVpc.publicSubnetIds,
-  // Private subnets will be used for cluster nodes
-  privateSubnetIds: eksVpc.privateSubnetIds,
-  accessEntries: {
-    instances: {
-      principalArn: role.arn,
-      type: eks.AccessEntryType.EC2Linux,
-    }
-  },
-  // Needed for AL2023 until the CNI is managed with an addon because our custom CNI is too old
-  useDefaultVpcCni: true,
-})
-
-// Note: Cluster that is passed an instanceProfileName via args
-// and that does not create a default NodeGroupV2 not tested as in this case
-// the instanceProfileName is ignored and not passed as an output
-
 
 const capacity = {
   desiredCapacity: 1,
   minSize: 1,
   maxSize: 1,
 }
-
-// NodeGroup //
-
-const ng_instanceProfileFromNgArgs = new eks.NodeGroup("ng-instanceProfileFromNgArgs", {
-  ...capacity,
-  cluster: cluster_NoIp_NoIpn,
-  instanceType: "t3.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
-  instanceProfile: instanceProfile,
-});
 
 const ng_instanceProfileNameFromNgArgs = new eks.NodeGroup("ng-instanceProfileNameFromNgArgs", {
   ...capacity,
@@ -111,51 +48,12 @@ const ng_instanceProfileNameFromNgArgs = new eks.NodeGroup("ng-instanceProfileNa
   instanceProfileName: instanceProfile.name,
 });
 
-const ng_instanceProfileFromClusterNgOpts = new eks.NodeGroup("ng-instanceProfileFromClusterNgOpts", {
-  ...capacity,
-  cluster: cluster_instanceProfileFromNgOpts,
-  instanceType: "t3.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
-});
-
-const ng_instanceProfileNameFromClusterNgOpts = new eks.NodeGroup("ng-instanceProfileNameFromClusterNgOpts", {
-  ...capacity,
-  cluster: cluster_instanceProfileNameFromNgOpts,
-  instanceType: "t3.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
-});
-
-
-// NodeGroupV2 //
-
-const ngv2_instanceProfileFromNgArgs = new eks.NodeGroupV2("ngv2-instanceProfileFromNgArgs", {
-  ...capacity,
-  cluster: cluster_NoIp_NoIpn,
-  instanceType: "t3.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
-  instanceProfile: instanceProfile,
-});
-
 const ngv2_instanceProfileNameFromNgArgs = new eks.NodeGroupV2("ngv2-instanceProfileNameFromNgArgs", {
   ...capacity,
   cluster: cluster_NoIp_NoIpn,
   instanceType: "t3.medium",
   operatingSystem: eks.OperatingSystem.AL2023,
   instanceProfileName: instanceProfile.name,
-});
-
-const ngv2_instanceProfileFromClusterNgOpts = new eks.NodeGroupV2("ngv2-instanceProfileFromClusterNgOpts", {
-  ...capacity,
-  cluster: cluster_instanceProfileFromNgOpts,
-  instanceType: "t3.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
-});
-
-const ngv2_instanceProfileNameFromClusterNgOpts = new eks.NodeGroupV2("ng-instanceProfileNameFromClusterNgOpts", {
-  ...capacity,
-  cluster: cluster_instanceProfileNameFromNgOpts,
-  instanceType: "t4g.medium",
-  operatingSystem: eks.OperatingSystem.AL2023,
 });
 
 export const passedInstanceProfileName = instanceProfile.name
