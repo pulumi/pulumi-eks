33ac95d73732a5aa560ea54e0bbc8c5517f05f3d
Florian Stadler <florian@pulumi.com>
2024-12-16 10:30:07 +0100

    Upgrade pu-aws to 6.65.0

--- go.mod
+++ go.mod
@@ -13,9 +13,10 @@ require (
 	github.com/pulumi/providertest v0.0.15
 	github.com/pulumi/pulumi-aws/sdk/v6 v6.41.0
 	github.com/pulumi/pulumi-eks/sdk/v3 v3.0.0-alpha.6
-	github.com/pulumi/pulumi/pkg/v3 v3.138.0
-	github.com/pulumi/pulumi/sdk/v3 v3.138.0
+	github.com/pulumi/pulumi/pkg/v3 v3.143.0
+	github.com/pulumi/pulumi/sdk/v3 v3.143.0
 	github.com/stretchr/testify v1.9.0
+	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8
 	golang.org/x/mod v0.20.0
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.29.3
@@ -183,14 +184,13 @@ require (
 	go.uber.org/atomic v1.11.0 // indirect
 	gocloud.dev v0.37.0 // indirect
 	gocloud.dev/secrets/hashivault v0.37.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/oauth2 v0.22.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/term v0.23.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
--- tests/testdata/programs/auto-mode-upgrade/step3/index.ts
+++ tests/testdata/programs/auto-mode-upgrade/step3/index.ts
@@ -1,6 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as awsx from "@pulumi/awsx";
-import * as aws from "@pulumi/aws";
 import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes";
 import { SubnetType } from "@pulumi/awsx/ec2";
@@ -18,23 +17,6 @@ const eksVpc = new awsx.ec2.Vpc("auto-mode-upgrade", {
     subnetStrategy: "Auto"
 });
 
-const nodeRole = new aws.iam.Role("eksNodeRole", {
-    assumeRolePolicy: aws.iam.getPolicyDocumentOutput({
-        statements: [{
-            actions: ["sts:AssumeRole"],
-            effect: "Allow",
-            principals: [{
-                identifiers: ["ec2.amazonaws.com"],
-                type: "Service",
-            }],
-        }],
-    }).json,
-    managedPolicyArns: [
-        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy",
-        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-    ],
-});
-
 const cluster = new eks.Cluster("auto-mode-upgrade", {
     name: clusterName,
     skipDefaultNodeGroup: true,
@@ -45,96 +27,11 @@ const cluster = new eks.Cluster("auto-mode-upgrade", {
     privateSubnetIds: eksVpc.privateSubnetIds,
     autoMode: {
         enabled: true,
-        // An upstream bug currently prevents setting a node role for existing clusters: https://github.com/pulumi/pulumi-aws/issues/4885
-        // This also prevents using the default node pools, so we must create a custom node pool.
-        createNodeRole: false,
-        computeConfig: {
-            nodePools: []
-        }
     },
-    accessEntries: {
-        nodeRole: {
-            principalArn: nodeRole.arn,
-            type: "EC2",
-            accessPolicies: {
-                autoNode: {
-                    policyArn: "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAutoNodePolicy",
-                    accessScope: {
-                        type: "cluster",
-                    },
-                }
-            }
-        }
-    }
 });
 
 export const nodeRoleName = cluster.autoModeNodeRoleName;
 export const kubeconfig = cluster.kubeconfig;
-
-const nodeClass = new k8s.apiextensions.CustomResource("node-class", {
-    apiVersion: "eks.amazonaws.com/v1",
-    kind: "NodeClass",
-    metadata: {
-        name: "private-compute",
-    },
-    spec: {
-        subnetSelectorTerms: [{
-            tags: {
-                [`kubernetes.io/cluster/${clusterName}`]: "shared",
-                "kubernetes.io/role/internal-elb": "1"
-            }
-        }],
-        securityGroupSelectorTerms: [{
-            id: cluster.eksCluster.vpcConfig.clusterSecurityGroupId,
-        }],
-        role: nodeRole.name,
-    },
-}, { provider: cluster.provider, retainOnDelete: true });
-
-const nodePool = new k8s.apiextensions.CustomResource("node-pool", {
-    apiVersion: "karpenter.sh/v1",
-    kind: "NodePool",
-    metadata: {
-        name: "default",
-    },
-    spec: {
-        template: {
-            spec: {
-                nodeClassRef: {
-                    group: "eks.amazonaws.com",
-                    kind: "NodeClass",
-                    name: nodeClass.metadata.name,
-                },
-                requirements: [
-                    {
-                        key: "eks.amazonaws.com/instance-category",
-                        operator: "In",
-                        values: ["c", "m", "r"],
-                    },
-                    {
-                        key: "eks.amazonaws.com/instance-cpu",
-                        operator: "In",
-                        values: ["4", "8", "16", "32"],
-                    },
-                    {
-                        key: "topology.kubernetes.io/zone",
-                        operator: "In",
-                        values: ["us-west-2a", "us-west-2b"],
-                    },
-                    {
-                        key: "kubernetes.io/arch",
-                        operator: "In",
-                        values: ["arm64", "amd64"],
-                    },
-                ],
-            },
-        },
-        limits: {
-            cpu: "50",
-            memory: "100Gi",
-        },
-    },
-}, { provider: cluster.provider, retainOnDelete: true });
 
 const appName = "nginx";
 const ns = new k8s.core.v1.Namespace(appName, {
