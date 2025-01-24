0a37dca74334b13566a12feb0c31ab11412fa418
Ramon Quitales <ramon@pulumi.com>
2025-01-24 13:43:59 -0800

    fix node build

--- provider/cmd/pulumi-resource-eks/main.go
+++ provider/cmd/pulumi-resource-eks/main.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This is an empty file to satisfy ci-mgmt constraints.
-
-//go:build ignore
+//go:generate go run ./generate.go
 
 package main
